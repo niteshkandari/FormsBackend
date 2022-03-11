3ec9a282d304d6ac8880170472d9986a48f2ab69
nitesh.kandari <nitesh.kandari@azilen.com>
2022-03-11 21:40:07 +0530

    fixed cors err

--- app.js
+++ app.js
@@ -1,44 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const formRoutes = require('./api/routes/formservice'); 
-const userRoutes = require('./api/routes/users');
+const formRoutes = require("./api/routes/formservice");
+const userRoutes = require("./api/routes/users");
 
-mongoose.connect('mongodb+srv://nitesh-user-1:'+ process.env.MONGO_ATLAS_PW +'@cluster0.dlsws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect(
+  "mongodb+srv://nitesh-user-1:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0.dlsws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
 
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(cors({ origin: "*" }));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin', '*'); 
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept,Authorization')
-    if(req.method === 'OPTIONS') { 
-       res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET')
-       return res.status(200).json({})
-    }
-    next();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
-app.use('/form',formRoutes);
-app.use('/user',userRoutes);
+app.use("/form", formRoutes);
+app.use("/user", userRoutes);
 
-app.use(function(req, res, next) {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
+app.use(function (req, res, next) {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
-app.use((error,req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error:{
-            message: error.message
-        }
-    })
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
-
 
 module.exports = app;
