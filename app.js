e14a0f700e2b19f66593def6ad6b18fb48e369fa
nitesh.kandari <nitesh.kandari@azilen.com>
2022-03-12 16:24:56 +0530

    changes

--- app.js
+++ app.js
@@ -17,21 +17,22 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(cors({origin:'*',methods:['PUT','POST','PATCH','DELETE','GET']})); 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept,Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
 
+app.use(cors({origin:'*',methods:['PUT','POST','PATCH','DELETE','GET']})); 
+  
 app.use("/form", formRoutes);
 app.use("/user", userRoutes);
 
